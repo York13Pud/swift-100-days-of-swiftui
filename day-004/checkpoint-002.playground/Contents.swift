1a68dda315a0c32efdcbfb2d24436ae1d2ae53ef
York13Pud <neilallwood@icloud.com>
2023-06-07 18:43:12 +0100

    Updated code

--- day-004/checkpoint-002.playground/Contents.swift
+++ day-004/checkpoint-002.playground/Contents.swift
@@ -1,3 +1,11 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let names: [String] = ["Fred", "Jane", "Sam", "Sam"]
+
+print(names)
+print(names.count)
+
+let nameSet: Set<String> = Set(names)
+
+print(nameSet)
+print(nameSet.count)
--- day-004/day-004.playground/Contents.swift
+++ day-004/day-004.playground/Contents.swift
@@ -1,4 +1,23 @@
 import Cocoa
 
-let surname:String = "Lasso"
-var score:Int = 0
+// Specify data types when creating a var or const:
+let surname: String = "Lasso"
+var score: Int = 0
+var scoreStart: Double = 0 // Even though it is an int, it will be set to 0.00
+var isAuthenticated: Bool = true
+
+// An array:
+var albums: [String] = ["Red", "Fearless"]
+var teams: [String] = [String]() // An empty array
+
+// A dictionary:
+var user: [String: String] = ["id": "@twostraws"]
+
+// A set:
+var books: Set<String> = Set(["The Bluest Eye", "Foundation", "Girl, Woman, Other"])
+
+// A constant can be defined without an initial value so it can be set later on. For example:
+let username: String
+username = "@fred"
+
+print(username)
