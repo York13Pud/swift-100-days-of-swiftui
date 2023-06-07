aa89acbdec20a2cfd458b949bfe58417a2c4a8a6
York13Pud <neilallwood@icloud.com>
2023-06-07 11:06:00 +0100

    Updated code

---
+++ day-003/day-003.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+// Creating arrays:
+var beatles = ["John", "Paul", "George", "Ringo"]
+let numbers = [4, 8, 15, 16, 23, 42]
+var temperatures = [25.3, 28.2, 26.4]
+var scores = Array<Int>() // Creates an empty array
+
+// View items in an array:
+print(beatles[0])
+print(numbers[1])
+print(temperatures[2])
