a73af7bedc79e8a6da25478647332f9ab9719405
York13Pud <neilallwood@icloud.com>
2023-06-16 11:27:36 +0100

    Updated code

---
+++ day-010/day-010.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+// A simple struct:
+
+struct Album {
+    let title: String
+    let artist: String
+    let year: Int
+
+    func printSummary() {
+        print("\(title) (\(year)) by \(artist)")
+    }
+}
+
+let red = Album(title: "Red", artist: "Taylor Swift", year: 2012)
+let wings = Album(title: "Wings", artist: "BTS", year: 2016)
+
+print(red.title)
+print(wings.artist)
+
+red.printSummary()
+wings.printSummary()
+print(red.self)
