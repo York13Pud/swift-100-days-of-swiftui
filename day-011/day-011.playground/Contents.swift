a223296283bf5f95e81c5b0e2cd6103dd64e68da
York13Pud <neilallwood@icloud.com>
2023-06-17 17:13:35 +0100

    Updated code

---
+++ day-011/day-011.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Cocoa
+
+struct BankAccount {
+    private var funds = 0
+
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 300)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+
+// This will not work as the funds variable is marked as private so it is not accessible
+// outside of the struct.
+// print(account.funds)
+
