28944b852268d13c0dd3b7e1a05181020500d320
York13Pud <neilallwood@icloud.com>
2023-06-21 12:58:31 +0100

    Updated code

--- day-014-optionals/day-014.playground/Contents.swift
+++ day-014-optionals/day-014.playground/Contents.swift
@@ -1,2 +1,20 @@
 import Cocoa
 
+let opposites = [
+    "Mario": "Wario",
+    "Luigi": "Waluigi"
+]
+
+// if let will check if the key "Mario" exists. If not, do nothing. If so, print:
+if let marioOpposite = opposites["Mario"] {
+    print("Mario's opposite is \(marioOpposite)")
+}
+
+
+var username: String? = nil
+
+if let unwrappedName = username {
+    print("We got a user: \(unwrappedName)")
+} else {
+    print("The optional was empty.")
+}
