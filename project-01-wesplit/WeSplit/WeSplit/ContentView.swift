2ba407fa6d0392cc1968e8f1a0a5f36592770b89
York13Pud <neilallwood@icloud.com>
2023-06-22 15:37:58 +0100

    Updated code

--- project-01-wesplit/WeSplit/WeSplit/ContentView.swift
+++ project-01-wesplit/WeSplit/WeSplit/ContentView.swift
@@ -11,33 +11,49 @@ struct ContentView: View {
     @State private var tapCount = 0
     @State private var name = ""
     
+    let characters = ["Ryu", "Ken", "Guile", "Chun-Li", "Zangief", "Dhalsim", "Blanka", "E. Honda"]
+    @State private var selectedCharacter = "Ryu"
+    
     var body: some View {
         VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
             NavigationView {
                 Form {
-                    TextField("Enter your name", text: $name)
-                    Text("Your name is \(name)")
                     Section {
-                        Text("Hello, world!")
-                        Text("Hello, world!")
+                        TextField("Enter your name", text: $name)
+                        Text("Your name is \(name)")
                     } header: {
-                        Text("This is a header")
+                        Text("An example of a textfield")
+                    }
+                    
+                    Section {
+                        Picker("Select your character", selection: $selectedCharacter) {
+                            ForEach(characters, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                    } header: {
+                        Text("An example of a picker")
                     }
                     
                     Section {
                         Text("Hello, world!")
-                        Text("Hello, world!")
                     } header: {
-                        Text("This is another header")
+                        Text("An example of a text box")
                     } footer: {
                         Text("Here is a footer of text")
                     }
+                    
+                    Section {
+                        ForEach(1..<11) { number in
+                            Text("Row \(number)")
+                        }
+                    } header: {
+                        Text("An example of a foreach loop")
+                    }
                 }
-                .navigationTitle("SwiftUI")
+                .navigationTitle("Example Elements")
             }
+            
             Button("Tap Count \(tapCount)") {
                 tapCount += 1
             }
