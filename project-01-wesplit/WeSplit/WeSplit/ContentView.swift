40705360fadd9d0fc596742f8d2f654d8c414aa5
York13Pud <neilallwood@icloud.com>
2023-06-23 19:36:08 +0100

    Updated code

--- project-01-wesplit/WeSplit/WeSplit/ContentView.swift
+++ project-01-wesplit/WeSplit/WeSplit/ContentView.swift
@@ -10,24 +10,90 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount: Double = 0.0
     @State private var numberOfPeople: Int = 2
-    @State private var tipPercentage: Int = 20
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     private let tipPercentages: [Int] = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Array<Double> {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = tipSelection * (checkAmount / 100)
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return [amountPerPerson, tipValue, grandTotal]
+    }
     
     var body: some View {
         VStack {
             NavigationView {
                 Form {
                     Section {
-                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                            .keyboardType(.decimalPad)
+                        HStack {
+                            Text("Enter Bill Amount")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .keyboardType(.decimalPad)
+                                .focused($amountIsFocused)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        
+//                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+//                            .keyboardType(.decimalPad)
                         Picker("Number of people", selection: $numberOfPeople) {
                             ForEach(2..<100) {
-                                Text("\($0) people")
+                                Text("\($0) People")
                             }
                         }
                     }
-                }.navigationTitle("We Split")
+                    
+                    Section {
+                        Picker("Tip percentage", selection: $tipPercentage) {
+                            ForEach(tipPercentages, id: \.self) {
+                                Text($0, format: .percent)
+                            }
+                        } .pickerStyle(.segmented)
+                    } header: {
+                        Text("Tip Amount")
+                    }
+                    
+                    Section {
+                        HStack {
+                            Text("Total Tip Value")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("\(totalPerPerson[1], format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        
+                        HStack {
+                            Text("Total Bill Value")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("\(totalPerPerson[2], format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        
+                        HStack {
+                            Text("Total Per Person")
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                            Text("\(totalPerPerson[0], format: .currency(code: Locale.current.currency?.identifier ?? "USD"))")
+                                .frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                        
+                    } header: {
+                        Text("Totals")
+                    }
+                    
+                } .navigationTitle("We Split")
+                    .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            Spacer()
+                            Button("Done") {
+                                amountIsFocused = false
+                            }
+                        }
+                    }
             }
         }
         .padding()
