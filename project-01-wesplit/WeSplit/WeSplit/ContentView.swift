a6e97bc0d2fb1cf2dd4c7957aad579404d8cc63a
York13Pud <neilallwood@icloud.com>
2023-06-23 10:51:15 +0100

    Updated code

--- project-01-wesplit/WeSplit/WeSplit/ContentView.swift
+++ project-01-wesplit/WeSplit/WeSplit/ContentView.swift
@@ -8,54 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
-    @State private var name = ""
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 20
     
-    let characters = ["Ryu", "Ken", "Guile", "Chun-Li", "Zangief", "Dhalsim", "Blanka", "E. Honda"]
-    @State private var selectedCharacter = "Ryu"
+    private let tipPercentages: [Int] = [10, 15, 20, 25, 0]
     
     var body: some View {
         VStack {
-            NavigationView {
-                Form {
-                    Section {
-                        TextField("Enter your name", text: $name)
-                        Text("Your name is \(name)")
-                    } header: {
-                        Text("An example of a textfield")
-                    }
-                    
-                    Section {
-                        Picker("Select your character", selection: $selectedCharacter) {
-                            ForEach(characters, id: \.self) {
-                                Text($0)
-                            }
-                        }
-                    } header: {
-                        Text("An example of a picker")
-                    }
-                    
-                    Section {
-                        Text("Hello, world!")
-                    } header: {
-                        Text("An example of a text box")
-                    } footer: {
-                        Text("Here is a footer of text")
-                    }
-                    
-                    Section {
-                        ForEach(1..<11) { number in
-                            Text("Row \(number)")
-                        }
-                    } header: {
-                        Text("An example of a foreach loop")
-                    }
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
-                .navigationTitle("Example Elements")
-            }
-            
-            Button("Tap Count \(tapCount)") {
-                tapCount += 1
             }
         }
         .padding()
