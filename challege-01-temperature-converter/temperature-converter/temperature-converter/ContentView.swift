37ae0e43a19aed63eff08ac902ea495bfa5f61cb
York13Pud <neilallwood@icloud.com>
2023-06-25 18:40:13 +0100

    Initial commit of challenge 1 project

---
+++ challege-01-temperature-converter/temperature-converter/temperature-converter/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  temperature-converter
+//
+//  Created by Neil Allwood on 25/06/2023.
+//
+// Objectives:
+// =============
+// 1. Get the user to input the temperature).
+// 2. User specifies the units (c, f or kelvin).
+// 3. Show conversion to the other units).
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ challege-01-temperature-converter/temperature-converter/temperature-converter/temperature_converterApp.swift
@@ -0,0 +1,17 @@
+//
+//  temperature_converterApp.swift
+//  temperature-converter
+//
+//  Created by Neil Allwood on 25/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct temperature_converterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
