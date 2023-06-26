f23a1499cdaf20d31d2764736b8f3ef951723d91
York13Pud <neilallwood@icloud.com>
2023-06-26 13:06:07 +0100

    Completed v1 of challenge. A little out of scope.

--- challege-01-temperature-converter/temperature-converter/temperature-converter/ContentView.swift
+++ challege-01-temperature-converter/temperature-converter/temperature-converter/ContentView.swift
@@ -14,10 +14,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var temperature: Double = 0
-    private let temperatureUnits: Array = ["C", "F", "Kel"]
+    private let temperatureUnits: Array = ["Celsius", "Fahrenheit", "Kelvin"]
+
+    @State private var temperature: Double = 0.00
+    @State private var temperatureUnit: String = "Celsius"
 
     @FocusState private var isFocused: Bool
+    
+    var convertedTemperatures: Dictionary<String, Double> {
+        var convertedC: Double = 0
+        var convertedF: Double = 0
+        var convertedKelvin: Double = 0
+        
+        if temperatureUnit == "Celsius" {
+            convertedC = temperature
+            convertedF = (temperature * 9/5) + 32
+            convertedKelvin = temperature + 273.15
+        } else if temperatureUnit == "Fahrenheit" {
+            convertedC = (temperature - 32) * 5/9
+            convertedF = temperature
+            convertedKelvin = (temperature - 32) * 5/9 + 273.15
+        } else if temperatureUnit == "Kelvin" {
+            convertedC = temperature - 273.15
+            convertedF = (temperature - 273.15) * 9/5 + 32
+            convertedKelvin = temperature
+        }
+        
+        return ["Celsius": convertedC, "Fahrenheit": convertedF, "Kelvin": convertedKelvin]
+    }
     
     var body: some View {
         VStack {
@@ -26,17 +50,36 @@ struct ContentView: View {
                     Section {
                         HStack {
                             Text("Enter Temperature") .frame(maxWidth: .infinity, alignment: .leading)
-                            TextField("Amount", value: $temperature, format: .number)
+                            TextField("Temperature", value: $temperature, format: .number)
                                 .keyboardType(.decimalPad)
                                 .focused($isFocused)
                                 .multilineTextAlignment(.trailing)
-                            
-                            
-                        }
+                            }
+                        
+                            Picker("Convert From", selection: $temperatureUnit) {
+                                ForEach(temperatureUnits, id: \.self ) {
+                                    Text($0)
+                                }
+                            }
                     } header: {
                         Text("Convert From")
                     }
-                } .navigationTitle("We Split")
+                    
+                    Section {
+                        ForEach(Array(convertedTemperatures.keys.sorted()), id: \.self) {key in
+                            if key != temperatureUnit {
+                                HStack {
+                                    Text("Converted To \(key)") .frame(maxWidth: .infinity, alignment: .leading)
+                                    Text("\(convertedTemperatures[key] ?? 0.0, specifier: "%.2f")")
+                                }
+                            }
+                        }
+                        
+                    } header: {
+                        Text("Converted Temperatures")
+                    }
+                    
+                } .navigationTitle("Temperature Converter")
                     .toolbar {
                         ToolbarItemGroup(placement: .keyboard) {
                             Spacer()
