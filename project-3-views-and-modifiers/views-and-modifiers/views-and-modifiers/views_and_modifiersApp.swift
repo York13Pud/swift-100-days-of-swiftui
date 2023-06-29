2e2da3991490610a66e942f5c65129cc5e52d680
York13Pud <neilallwood@icloud.com>
2023-06-29 10:14:24 +0100

    Started project 3

---
+++ project-3-views-and-modifiers/views-and-modifiers/views-and-modifiers/views_and_modifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  views_and_modifiersApp.swift
+//  views-and-modifiers
+//
+//  Created by Neil Allwood on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct views_and_modifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
