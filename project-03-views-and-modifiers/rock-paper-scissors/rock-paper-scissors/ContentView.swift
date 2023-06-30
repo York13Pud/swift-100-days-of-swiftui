ac618d6d3ba68e4cbce92a8759dc1e70becbc0ce
York13Pud <neilallwood@icloud.com>
2023-06-30 15:20:40 +0100

    Updated code

--- project-03-views-and-modifiers/rock-paper-scissors/rock-paper-scissors/ContentView.swift
+++ project-03-views-and-modifiers/rock-paper-scissors/rock-paper-scissors/ContentView.swift
@@ -7,21 +7,130 @@
 
 import SwiftUI
 
-let choices: Array<String> = ["rock", "paper", "scissors"]
+let choice: Array<String> = ["rock", "paper", "scissors"]
+let choiceEmoji: Array<String> = ["✊", "✋", "✌️"]
+let choiceBeats: Array<String> = ["scissors", "rock", "paper"]
 
 struct ContentView: View {
     @State var playerChoice:String = ""
     @State var computerChoice:String = ""
+    
+    @State var playerScore:Int = 0
+    @State var computerScore:Int = 0
+    
     @State var showAlert: Bool = false
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.cyan, .purple]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+         
+            VStack {
+                
+                // App title:
+                Text("rock - paper - scissors".capitalized)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                // End of app title.
+                
+                // Player choice area:
+                VStack {
+                    
+                    Spacer()
+                    
+                    Text("Select your move".capitalized)
+                        .font(.title)
+                        .foregroundColor(.white)
+                    
+                    HStack {
+                        Button {
+                            print("rock")
+                        }
+                        label: {
+                            VStack {
+                                Text("✊")
+                                    .font(.largeTitle)
+                                Text("Rock")
+                                    .foregroundColor(.primary)
+                            }
+                            
+                        }.frame(maxWidth: .infinity)
+                        .padding(.vertical, 20)
+                        .background(.regularMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                        
+                        Spacer()
+                        
+                    Button {
+                            print("paper")
+                        }
+                        label: {
+                            VStack {
+                                Text("✋")
+                                    .font(.largeTitle)
+                                Text("Paper")
+                                    .foregroundColor(.primary)
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
+                            .padding(.vertical, 20)
+                            .background(.regularMaterial)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+
+                    Spacer()
+                    
+                    Button {
+                        print("Scissors")
+                    }
+                    label: {
+                        VStack {
+                            Text("✌️")
+                                .font(.largeTitle)
+                            Text("Scissors")
+                                .foregroundColor(.primary)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                        .padding(.vertical, 20)
+                        .background(.regularMaterial)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    }
+                    // End of player choice area.
+
+                    Spacer()
+                    
+                    // Scores area:
+                    VStack {
+
+                        HStack {
+                            
+                            VStack {
+                                Text("Player".capitalized)
+                                Text("\(playerScore)".capitalized)
+                            }
+                            Spacer()
+                            
+                            VStack {
+                                Text("Scores".capitalized)
+                                Text(" ")
+                            }
+                            
+                            Spacer()
+                            VStack {
+                                Text("Device".capitalized)
+                                Text("\(computerScore)".capitalized)
+                            }
+                            
+                        }
+                    }.font(.title)
+                        .foregroundColor(.white)
+                    // End of scores area.
+                    
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
