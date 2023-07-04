e1ac04984443c4db9f3805b0756a790d5673628b
York13Pud <neilallwood@icloud.com>
2023-07-04 10:27:48 +0100

    Started project 4

---
+++ project-04-better-rest/better-rest/better-rest/better_restApp.swift
@@ -0,0 +1,17 @@
+//
+//  better_restApp.swift
+//  better-rest
+//
+//  Created by Neil Allwood on 04/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct better_restApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
