7639307c2be78231890c17c1f47268ea65e08349
York13Pud <neilallwood@icloud.com>
2023-07-06 13:48:13 +0100

    Updated code

--- project-05-word-scramble/word-scramble/word-scramble/ContentView.swift
+++ project-05-word-scramble/word-scramble/word-scramble/ContentView.swift
@@ -15,6 +15,20 @@ struct ContentView: View {
         let input = "a,b,c"
         let letters = input.components(separatedBy: ",")
         print(letters)
+        
+        let letter = letters.randomElement() // Get a single character
+        print(letter ?? "a")
+        
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines) // Remove whitespace
+        print(trimmed ?? "")
+        
+        // Run a word through a spell checker:
+        let word = "swift"
+        let checker = UITextChecker() // The spell checker
+        let range = NSRange(location: 0, length: word.utf16.count) // Specify the length of the word to check
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound
+        print(allGood) // Returns true if word is spelled correctly, false otherwise.
     }
     
     var body: some View {
