9b7aeb57cff8845169af43f3098fea0d47166899
York13Pud <neilallwood@icloud.com>
2023-07-10 10:56:19 +0100

    Updated code

--- project-06-animations/animations/animations/ContentView.swift
+++ project-06-animations/animations/animations/ContentView.swift
@@ -8,19 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount1 = 1.0
+    @State private var animationAmount2 = 1.0
+    @State private var animationAmount3 = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            animationAmount += 0.25
+        VStack {
+            Button("Tap Me") {
+                animationAmount1 += 0.25
+            }
+            .padding(50)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount1) // Adds a scale factor to increase the size of the button
+            .animation(.default,
+                       value: animationAmount1) // Animates the size increase. .default has many alternatives.
+            //.blur(radius: (animationAmount - 1) * 3)
+            
+            Button("Tap Me") {
+                animationAmount2 += 0.25
+            }
+            .padding(50)
+            .background(.blue)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount2)
+            .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true),
+                       value: animationAmount2)
+            
+            Button("Tap Me") {
+                animationAmount3 += 0.25
+            }
+            .padding(50)
+            .background(.green)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(.green)
+                    .scaleEffect(animationAmount3)
+                    .opacity(2 - animationAmount3)
+                    .animation(
+                        .easeOut(duration: 1)
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount3
+                    )
+            )
+            .onAppear {
+                animationAmount3 = 2
+            }
+            .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true),
+                       value: animationAmount2)
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .scaleEffect(animationAmount) // Adds a scale factor to increase the size of the button
-        .animation(.default, value: animationAmount) // Animates the size increase
-        //.blur(radius: (animationAmount - 1) * 3)
     }
 }
 
