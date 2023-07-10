7793561dbebd06102faaf1ae444d7f7853c7d560
York13Pud <neilallwood@icloud.com>
2023-07-10 13:44:30 +0100

    Started project 7 iexpense

---
+++ project-07-iexpense/iexpense/iexpense/iexpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iexpenseApp.swift
+//  iexpense
+//
+//  Created by Neil Allwood on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct iexpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
