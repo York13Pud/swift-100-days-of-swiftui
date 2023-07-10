c751779f111d1548b7dc647952a27a2dc1974835
York13Pud <neilallwood@icloud.com>
2023-07-10 11:35:44 +0100

    Updated code

--- project-06-animations/animations/animations/ContentView.swift
+++ project-06-animations/animations/animations/ContentView.swift
@@ -11,31 +11,35 @@ struct ContentView: View {
     @State private var animationAmount1 = 1.0
     @State private var animationAmount2 = 1.0
     @State private var animationAmount3 = 1.0
+    @State private var animationAmount4 = 1.0
+    @State private var animationAmount5 = 0.0
     
     var body: some View {
         VStack {
-            Button("Tap Me") {
-                animationAmount1 += 0.25
+            HStack {
+                Button("Tap Me") {
+                    animationAmount1 += 0.25
+                }
+                .padding(50)
+                .background(.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(animationAmount1) // Adds a scale factor to increase the size of the button
+                .animation(.default,
+                           value: animationAmount1) // Animates the size increase. .default has many alternatives.
+                //.blur(radius: (animationAmount - 1) * 3)
+                
+                Button("Tap Me") {
+                    animationAmount2 += 0.25
+                }
+                .padding(50)
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(animationAmount2)
+                .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true),
+                           value: animationAmount2)
             }
-            .padding(50)
-            .background(.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount1) // Adds a scale factor to increase the size of the button
-            .animation(.default,
-                       value: animationAmount1) // Animates the size increase. .default has many alternatives.
-            //.blur(radius: (animationAmount - 1) * 3)
-            
-            Button("Tap Me") {
-                animationAmount2 += 0.25
-            }
-            .padding(50)
-            .background(.blue)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount2)
-            .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true),
-                       value: animationAmount2)
             
             Button("Tap Me") {
                 animationAmount3 += 0.25
@@ -59,7 +63,46 @@ struct ContentView: View {
                 animationAmount3 = 2
             }
             .animation(.easeInOut(duration: 2).repeatForever(autoreverses: true),
-                       value: animationAmount2)
+                       value: animationAmount3)
+            
+            Button("Tap Me") {
+                        // do nothing
+                    }
+                    .padding(50)
+                    .background(.purple)
+                    .foregroundColor(.white)
+                    .clipShape(Circle())
+            
+            HStack {
+                Spacer()
+                
+                Stepper("Scale amount", value: $animationAmount4.animation(), in: 1...2)
+                    .labelsHidden()
+                
+                Spacer()
+                
+                Button("Tap Me") {
+                    animationAmount4 += 0.25
+                }
+                .padding(40)
+                .background(.indigo)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(animationAmount4)
+                
+                Spacer()
+            }
+            
+            Button("Tap Me") {
+                withAnimation(.interpolatingSpring(stiffness: 3, damping: 1)) {
+                    animationAmount5 += 360
+                }
+            }
+            .padding(50)
+            .background(.black)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount5), axis: (x: 0, y: 1, z: 1))
         }
     }
 }
